361d902a274a978f097b50bda0c9708e24e0cbca
leodoi3 <46114199+leodoi3@users.noreply.github.com>
2021-07-03 09:35:57 -0700

    Add files via upload

--- closeTab.js
+++ closeTab.js
@@ -1,18 +1,51 @@
+
 function checkAndCloseTab(tabId, changeInfo, tab) {
   console.log("checkAndCloseTab");
-  browser.storage.local.get("title_regex").then(checkRegex);
+  browser.storage.local.get("close_regex").then(checkRegex);
 
   function checkRegex(result) {
     console.log(result);
-    var patt = new RegExp(result.title_regex);
-    if (result.title_regex && result.title_regex.length > 0 && tab && patt.test(tab.title)) {
+    var patt = new RegExp(result.close_regex);
+    if (result.close_regex && result.close_regex.length > 0 && tab && patt.test(tab.title)) {
       chrome.tabs.remove(tabId);
     }
   }
 }
+function handleClick() {
+    browser.runtime.openOptionsPage();
+}
+
+
+
+ function checkAndReloadTab(tabId, changeInfo, tab) {
+    console.log("checkAndReloadTab");
+     browser.storage.local.get("reload_regex").then(checkRegex);
+
+    async function checkRegex(result) {
+        console.log(result);
+        var patt = new RegExp(result.reload_regex);
+        if (result.reload_regex && result.reload_regex.length > 0 && tab && patt.test(tab.title) && changeInfo.status == 'complete') {
+     //       var allTabs = (await browser.tabs.query({})).length
+       //     console.log("tab length: "+allTabs);
+        //    await new Promise(r => setTimeout(r, 1000 * allTabs));
+                chrome.tabs.reload(tabId);
+           
+        }
+    }
+}
 
 // listen to tab URL changes
 chrome.tabs.onUpdated.addListener(checkAndCloseTab);
+chrome.tabs.onUpdated.addListener(checkAndReloadTab);
+//Object.addListener('load',checkAndReloadTab );
+//document.addEventListener('DOMContentLoaded', checkAndReloadTab, false);
+//chrome.webRequest.onCompleted.addListener(checkAndReloadTab);
+//chrome.tabs.webNavigation.onCompleted.addListener(checkAndReloadTab);
+//chrome.tabs.webNavigation.onCompleted(checkAndReloadTab);
+
 
 // update when the extension loads initially
 checkAndCloseTab();
+checkAndReloadTab();
+
+browser.browserAction.onClicked.addListener(handleClick);
--- settings.js
+++ settings.js
@@ -5,20 +5,27 @@ document.querySelector("form").addEventListener("submit", saveOptions);
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.local.set({
-    title_regex: document.querySelector("#title_regex").value
+      close_regex: document.querySelector("#close_regex").value,
+      reload_regex: document.querySelector("#reload_regex").value
   });
 }
 
 function restoreOptions() {
 
   function setCurrentChoice(result) {
-    document.querySelector("#title_regex").value = result.title_regex || "Request blocked";
+      document.querySelector("#close_regex").value = result.close_regex || "Request blocked";
   }
+    function setCurrentChoice2(result) {
+        document.querySelector("#reload_regex").value = result.reload_regex || "Request blocked";
+    }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  var getting = browser.storage.local.get("title_regex");
-  getting.then(setCurrentChoice, onError);
+  var getting = browser.storage.local.get("close_regex");
+    getting.then(setCurrentChoice, onError);
+
+  var getting2 = browser.storage.local.get("reload_regex");
+    getting2.then(setCurrentChoice2, onError);
 }
